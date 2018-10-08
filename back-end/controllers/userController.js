4eaac110bf40a636ccabf7d3f518415eb012c391
FatimaAlmostafa <ff242468@gmail.com>
2018-10-08 23:34:23 +0300

    usercontroller

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,16 +1,28 @@
-
+var db=require('../models/User.js');
+var mongoose=require('mongoose');
 // Given the name of a user, retrieve their record from the database
 exports.getUserByName = function (name, callback) {
-  // TODO
+  db.User.findOne({name:name},function(err,data){
+    if (err) {
+      callback('err',null)
+    } else {
+      callback(null,data);
+    }
+  })
 };
 
 // Given the name of a user, update their `email` property
 exports.updateEmailByName = function (name, newEmail, callback) {
-  // TODO
-
+  db.User.findOneAndUpdate({name:name},{$set:newEmail},function(err,data) {
+    if (err) {
+      callback('err in ubdating')
+    } else {
+      callback('ubdating successfully');
+    }
+  })
 };
 
 // Read all users from the database at once
 exports.readAllUsers = function (callback) {
-  // TODO
+  db.User.find({}).pretty()
 };
